0aebdaee3597e2d5b15f7d057d1626e7f648220d
Raz Cohen <razcohenp@gmail.com>
2025-01-08 18:44:32 +0200

    commit
    commit

--- system/Librarian.py
+++ system/Librarian.py
@@ -1,6 +1,6 @@
 from books import *
 from books.BookFactory import BookFactory
-from system.Management import Management
+from system import shared
 from system.files_management import BooksFileManagement
 
 class Librarian:
@@ -11,22 +11,28 @@ class Librarian:
     def add_book(self, title, author, is_loaned, copies, genre, year):
         new_book = BookFactory.create_book(title, author, is_loaned, int(copies), BookType(genre), int(year))
 
-        found = False
-        for book in Management.books:
+        for book in shared.books:
+            # If book already exists
             if new_book.__eq__(book):
-                found = True
                 book.add_copy()
-                break
+                BooksFileManagement.update()
+                return
 
-        if found:
-            BooksFileManagement.update()
+        # If the book does not exist
+        shared.books.append(new_book)
+        BooksFileManagement.add_book(new_book)
 
-        else:
-            Management.books.append(new_book)
-            BooksFileManagement.add_book(new_book)
+    def remove_book(self, title, author, is_loaned, copies, genre, year):
+        book_to_remove = BookFactory.create_book(title, author, is_loaned, int(copies), BookType(genre), int(year))
+        for book in shared.books:
+            if book_to_remove.__eq__(book):
+                shared.books.remove(book_to_remove)
+                BooksFileManagement.update()
+                return
+        print("the book is not found")
 
-    def remove_book(self, title, author):
-        pass
+
+
 
 
 
--- system/Management.py
+++ system/Management.py
@@ -1,7 +1,5 @@
-from system.files_management.BooksFileManagement import BooksFileManagement
+from system.files_management.BooksFileManagement import setup
 
 class Management:
-    books = []
-
     def __init__(self):
-        BooksFileManagement.setup()
+        setup()
